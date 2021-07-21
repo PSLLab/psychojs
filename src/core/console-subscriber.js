590fe6c6851d0aa940490caec1814f0a2211fbed
Jarrod Moss <jarrod.moss@psllab.org>
2021-07-21 11:06:50 -0500

    add console subscriber to psychojs

---
+++ src/core/console-subscriber.js
@@ -0,0 +1,69 @@
+/**
+ * Subscribe to the console output
+ *
+ * @author Samson Radu
+ */
+const CATEGORY_INFO = "info";
+const CATEGORY_WARN = "warn";
+const CATEGORY_ERROR = "error";
+const CATEGORY_DEBUG = "debug";
+
+var _log = console.log;
+var _warn = console.warn;
+var _error = console.error;
+var _debug = console.debug;
+
+var ConsoleSubscriber = {
+    unbind: function(){
+        console.log = _log;
+        console.warn = _warn;
+        console.error = _error;
+        console.debug = _debug;
+    },
+    bind: function(cb, preventLogging){
+        if (typeof cb !== 'function'){
+            console.error("You must pass a valid callback function.");
+            return false;
+        }
+
+        preventLogging = Boolean(preventLogging); //force bool
+
+        console.log = console.info = function(){
+            if (_log){
+                if (!preventLogging){
+                    _log.call(console, ...arguments);
+                }
+                cb(CATEGORY_INFO, arguments);
+            }
+        };
+
+        console.warn = function(){
+            if (_warn){
+                if (!preventLogging){
+                    _warn.call(console, ...arguments);
+                }
+                cb(CATEGORY_WARN, arguments);
+            }
+        };
+
+        console.error = function(){
+            if (_error){
+                if (!preventLogging){
+                    _error.call(console, ...arguments);
+                }
+                cb(CATEGORY_ERROR, arguments);
+            }
+        };
+
+        console.debug = function(){
+            if (_debug){
+                if (!preventLogging){
+                    _debug.call(console, ...arguments);
+                }
+                cb(CATEGORY_DEBUG, arguments);
+            }
+        };
+    }
+};
+
+export { ConsoleSubscriber };
